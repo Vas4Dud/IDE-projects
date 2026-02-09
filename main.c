6914a40117cf7ce1ffd630b445005c45890c2681
Frogz9000 <ihurtmyknee11@gmail.com>
2026-02-09 11:25:11 -0800

    Genreated part 1

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <lab4\I2C\i2c.h>
 #include <lab4\I2C\oled.h>
-#include "lab1/switches.h"
+#include <lab1\switches.h>
 /**
  * ******************************************************************************
  * @file    : main.c
@@ -29,7 +29,28 @@ int main(void)
 
 void part1(void)
 {
-	
+	char* string1 = "Hello World";
+	unsigned int string1_len = strlen(string1);
+	char* string2 = "How Are You?";
+	unsigned int string2_len = strlen(string2);
+	char* string3 = "Goodbye";
+	unsigned int string3_len = strlen(string3);
+	char* string4 = "Sam and Vassily";
+	unsigned int string4_len = strlen(string4);
+
+	for(unsigned int j = 0; j < string1_len; j++){
+		OLED_draw(0,(int)j,string1[j]);
+	}
+	for(unsigned int j = 0; j < string2_len; j++){
+		OLED_draw(1,(int)j,string2[j]);
+	}
+	for(unsigned int j = 0; j < string3_len; j++){
+		OLED_draw(2,(int)j,string3[j]);
+	}
+	for(unsigned int j = 0; j < string4_len; j++){
+		OLED_draw(3,(int)j,string4[j]);
+	}
+
 }
 
 void part2(void)
