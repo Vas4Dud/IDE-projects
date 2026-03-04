c6d57f32b5e2d59d00168267865271325b11f691
Frogz9000 <ihurtmyknee11@gmail.com>
2026-03-04 13:41:23 -0800

    added dc motor controls

---
+++ lab6/DC_motor.h
@@ -0,0 +1,17 @@
+#ifndef _DC_MOTOR_H_
+#define _DC_MOTOR_H_
+
+#include <stdint.h>
+
+
+
+void init_dc_motor(uint32_t period, uint32_t prescaler, double percentDutyCycle);
+
+void dc_forward(double percentDutyCycle);
+
+void dc_backwards(double percentDutyCycle);
+
+
+
+
+#endif // _TIMERS_H_
